8eec7e82b59caf0b34d5c032178d39cf728dc609
juliantomlinson91 <124048762@upq.edu.mx>
2025-11-30 21:45:27 -0600

    App

--- CHEFBYTEPI/App.js
+++ CHEFBYTEPI/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import InicioSesionScreen from './screens/InicioSesionScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <InicioSesionScreen onLoginSuccess={() => console.log('Login Ok')} />
       <StatusBar style="auto" />
     </View>
   );
